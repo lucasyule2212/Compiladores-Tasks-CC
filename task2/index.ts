b16d7287e5cc634a001bcea57a89ca03703b770b
Lucas Yule <lyrma@cin.ufpe.br>
2023-04-20 19:07:47 -0300

    feat: Task 2 Compiladores

---
+++ task2/index.ts
@@ -0,0 +1,94 @@
+enum TokenType {
+  NUM = "NUM",
+  MINUS = "MINUS",
+  PLUS = "PLUS",
+  SLASH = "SLASH",
+  STAR = "STAR",
+  EOF = " EOF",
+}
+
+class Token {
+  public type: TokenType; // tipo do token
+  public lexeme: string; // valor do token
+
+  constructor(type: TokenType, value: string) {
+    this.type = type;
+    this.lexeme = value;
+  }
+
+  public toString(): string {
+    return `Token [type=${this.type}, lexeme=${this.lexeme}]`;
+  }
+}
+
+function scan(input: string): Token[] {
+  const lines = input.split("\n");
+  const tokens: Token[] = [];
+
+  for (const line of lines) {
+    const lexeme = line.trim();
+
+    if (lexeme === "") {
+      continue;
+    }
+
+    if (!isNaN(parseFloat(lexeme))) {
+      tokens.push(new Token(TokenType.NUM, lexeme));
+    } else if (lexeme === "+") {
+      tokens.push(new Token(TokenType.PLUS, lexeme));
+    } else if (lexeme === "-") {
+      tokens.push(new Token(TokenType.MINUS, lexeme));
+    } else if (lexeme === "*") {
+      tokens.push(new Token(TokenType.STAR, lexeme));
+    } else if (lexeme === "/") {
+      tokens.push(new Token(TokenType.SLASH, lexeme));
+    } else {
+      throw new Error(`Unexpected character: ${lexeme}`);
+    }
+  }
+
+  return tokens;
+}
+
+function operar(tokens: Token[]): number {
+  let pilha: number[] = [];
+
+  tokens.forEach(function (token) {
+    if (token.type === TokenType.NUM) {
+      pilha.push(parseFloat(token.lexeme));
+    } else {
+      let b: number = pilha.pop() as number;
+      let a: number = pilha.pop() as number;
+      switch (token.type) {
+        case TokenType.PLUS:
+          pilha.push(a + b);
+          break;
+        case TokenType.MINUS:
+          pilha.push(a - b);
+          break;
+        case TokenType.STAR:
+          pilha.push(a * b);
+          break;
+        case TokenType.SLASH:
+          pilha.push(a / b);
+          break;
+        default:
+          throw new Error("Operador inválido: " + token.lexeme);
+      }
+    }
+  });
+  return pilha.pop() as number;
+}
+
+let fs = require("fs");
+let input = fs.readFileSync("Calc1.stk", "utf-8");
+
+let tokens = scan(input);
+
+tokens.forEach((token) => {
+  console.log(token.toString());
+});
+
+const resultado = operar(tokens);
+
+console.log("Saída: " + resultado);
